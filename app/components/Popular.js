01980bebf04ababa24484f0dbbdc2d4f122c5af5
Curtis Vanzandt <ctvanzandt42@gmail.com>
2021-04-17 20:46:15 -0500

    Made Popular a functional component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-export default class Popular extends React.Component {
-    render() {
-        const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+const Popular = () => {
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
-        return (
-            <ul className="flex-center">
-                {languages.map(( language, index ) => (
-                    <li key={index}>
-                        <button className="btn-clear nav-link">
-                            {language}
-                        </button>
-                    </li>
-                ))}
-            </ul>
-        )
-    }
+    return (
+        <ul className="flex-center">
+            {languages.map(( language, index ) => (
+                <li key={index}>
+                    <button className="btn-clear nav-link">
+                        {language}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    )
 }
+
+export default Popular;
