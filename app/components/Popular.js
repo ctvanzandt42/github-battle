a7521bf0ad79fb5d9cc8c063f8775d31f1dc0560
Curtis Vanzandt <ctvanzandt42@gmail.com>
2021-04-25 16:58:11 -0500

    Added hook for state on popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Popular = () => {
+    const [selectedLanguage, setSelectedLanguage] = useState('All');
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+
+    const updateLanguage = (selectedLanguage) => {
+        setSelectedLanguage(selectedLanguage);
+    }
 
     return (
         <ul className="flex-center">
             {languages.map(( language, index ) => (
                 <li key={index}>
-                    <button className="btn-clear nav-link">
+                    <button
+                        className="btn-clear nav-link"
+                        style={language === selectedLanguage ? { color: 'rgb(187, 46, 31' } : null }
+                        onClick={() => updateLanguage(language)}>
                         {language}
                     </button>
                 </li>
