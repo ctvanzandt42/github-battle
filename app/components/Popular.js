bbccce5969523586d07bf90df45422e4ac91af84
Curtis Vanzandt <curtis.vanzandt@carecorenational.com>
2021-04-27 19:57:29 -0500

    Added prop-types package, added LanguageNav functional component and reference to the Popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,26 +1,17 @@
 import React, { useState } from "react";
+import LanguagesNav from "./LanguagesNav";
 
 const Popular = () => {
     const [selectedLanguage, setSelectedLanguage] = useState('All');
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
     const updateLanguage = (selectedLanguage) => {
         setSelectedLanguage(selectedLanguage);
     }
 
     return (
-        <ul className="flex-center">
-            {languages.map(( language, index ) => (
-                <li key={index}>
-                    <button
-                        className="btn-clear nav-link"
-                        style={language === selectedLanguage ? { color: 'rgb(187, 46, 31' } : null }
-                        onClick={() => updateLanguage(language)}>
-                        {language}
-                    </button>
-                </li>
-            ))}
-        </ul>
+        <LanguagesNav
+            onUpdateLanguage={updateLanguage}
+            selected={selectedLanguage} />
     )
 }
 
